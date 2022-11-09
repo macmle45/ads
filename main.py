bec4df71d5aa6b357f78c8204aa4ea9bbeb8fabc
macmle45 <maciej.mleczko.net@gmail.com>
2022-11-09 16:15:39 +0100

    operations on stack data structure

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+from lds import Stack
+
 
 def main():
-    pass
+    my_stack = Stack()
+    
+    print(my_stack.pop())
+    print(f"Is empty: {my_stack.is_empty()}")
+    my_stack.push(1)
+    print(my_stack.pop())
+    print(f"Stack: {my_stack}")
+    print(f"Is empty: {my_stack.is_empty()}")
+    my_stack.pop()
+    print(f"Is empty: {my_stack.is_empty()}")
+    my_stack.push(3)
+    my_stack.push(10)
+    my_stack.push(5)
+    my_stack.push(7)
+    print(f"Stack: {my_stack}")
+    print("Removing last item...")
+    my_stack.pop()
+    print(f"Stack: {my_stack}")
+
 
 
 if __name__ == '__main__':
