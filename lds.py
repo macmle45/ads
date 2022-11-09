8af4d99d9e7c8d60f74fef3b860744b8f0a4d391
macmle45 <maciej.mleczko.net@gmail.com>
2022-11-09 19:24:42 +0100

    added 'Queue' implementation

--- lds.py
+++ lds.py
@@ -2,23 +2,50 @@ class Stack:
     def __init__(self, stack=[]):
         self.stack = stack
 
+    # check if stack is empty
     def is_empty(self):
         return len(self.stack) == 0
 
+    # added alelemnt to the stack
     def push(self, element):
         return self.stack.append(element)
 
+    # remove first element from the stack
     def pop(self):
         if self.is_empty():
             return f"Stack is empty"
 
         return self.stack.pop()
 
+    # first element on the stack
     def peek(self):
         if self.is_empty():
             return f"Stack is empty"
-            
+
         return self.stack[len(self.stack)-1]
 
     def __str__(self) -> str:
         return f"{self.stack}"
+
+
+class Queue:
+    def __init__(self) -> None:
+        self.queue = []
+
+    # add element to the queue
+    def enqueue(self, element):
+        self.queue.append(element)
+    
+    #remove element from the queue
+    def dequeue(self):
+        if self.size == 0:
+            return "Queue is empty"
+        return self.queue.pop(0)
+    
+    # return size of queue
+    @property
+    def size(self):
+        return len(self.queue)
+
+    def __str__(self) -> str:
+        return f'{self.queue}'
