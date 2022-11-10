fc6a647f7bd5b6e092d3f4ee8a70b3b9b076c1a6
macmle45 <maciej.mleczko.net@gmail.com>
2022-11-10 22:52:56 +0100

    added implementation of 'Circular Queue'

--- lds.py
+++ lds.py
@@ -49,3 +49,47 @@ class Queue:
 
     def __str__(self) -> str:
         return f'{self.queue}'
+
+
+class CircularQueue:
+    def __init__(self, size):
+        self.size = size
+        self.circular_queue = [None] * size
+        self.head = -1
+        self.tail = -1
+
+    def enqueue(self, element):
+        if (self.tail + 1) % self.size == self.head:
+            return f'The circular queue is full'
+        elif self.head == -1:
+            self.head = 0
+            self.tail = 0
+            self.circular_queue[self.tail] = element
+        else:
+            self.tail = (self.tail + 1) % self.size
+            self.circular_queue[self.tail] = element
+
+    def dequeue(self):
+        if self.head == -1:
+            return f'The circular queue is empty'
+        elif self.head == self.tail:
+            temp = self.circular_queue[self.head]
+            self.head = -1
+            self.tail = -1
+            return temp
+        else:
+            temp = self.circular_queue[self.head]
+            self.head = (self.head + 1) % self.size
+            return temp
+
+    def __str__(self) -> str:
+        if self.head == -1:
+            return f'Circular queue is empty'
+        elif self.tail > self.head:
+            return f'{[self.circular_queue[i] for i in range(self.head, self.tail + 1)]}'
+        else:
+            cq_1 = [self.circular_queue[i] for i in range(self.head, self.size)]
+            cq_2 = [self.circular_queue[i] for i in range(0, self.tail + 1)]
+
+            return f'{[i for i in cq_2 + cq_1]}'
+
